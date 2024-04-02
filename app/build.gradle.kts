8f35442ca13cf2b23ffe137a4dfbf70093179cdb
e <minnki19@gmail.com>
2024-04-02 20:05:49 +0900

    add sparkline layout

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,7 +52,8 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
-    implementation("com.github.majorik:SparkLineLayout:1.0.1")
-    implementation("com.github.bumptech.glide:glide:4.8.0")
+    implementation ("com.github.majorkik:SparkLineLayout:1.0.1")
+    implementation ("com.github.bumptech.glide:glide:4.16.0")
+
 
 }
